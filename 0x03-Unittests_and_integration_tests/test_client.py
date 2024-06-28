9ff071cb438a686bb267fd3db4cd42c9734f59aa
Alex Kasema <lexxmulwa@gmail.com>
2024-06-28 05:19:54 -0700

    Implement the test_public_repos method to test GithubOrgClient.public_repos. Implement test_public_repos_with_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -114,7 +114,7 @@ class TestGithubOrgClient(unittest.TestCase):
 @parameterized_class([
     {
         "org_payload": TEST_PAYLOAD[0][0],
-        "repos_paylaod": TEST_PAYLOAD[0][1],
+        "repos_payload": TEST_PAYLOAD[0][1],
         "expected_repos": TEST_PAYLOAD[0][2],
         "apache2_repos": TEST_PAYLOAD[0][3],
     },
@@ -137,6 +137,20 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
 
         cls.get_patcher = patch("requests.get", side_effect=get_payload)
         cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """ test the GithubOrgClient.public_repos method """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """ test the GithubOrgClient.public_repos with a given license """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
 
     @classmethod
     def tearDownClass(cls) -> None:
